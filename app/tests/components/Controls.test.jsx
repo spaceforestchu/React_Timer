6f260901f1959e3cfb0c5f5ee6ffb3865599a51b
SpaceForestChu <spaceforestchu@gmail.com>
2016-09-13 12:00:56 -0400

    error control.test.jsx

---
+++ app/tests/components/Controls.test.jsx
@@ -0,0 +1,7 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var expect = require('expect');
+var $ = require('jQuery');
+var TestUtils = require('react-addons-test-utils');
+
+var Controls = require('Controls');
